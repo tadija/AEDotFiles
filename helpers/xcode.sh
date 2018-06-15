485dddf72f51e9ce29eb7e8e5a6fb7b8772c8755
Marko Tadic <tadija@me.com>
2018-06-15 21:48:23 +0200

    Added `xcwhich` and `xcswitch`

--- helpers/xcode.sh
+++ helpers/xcode.sh
@@ -3,6 +3,7 @@
 # Licensed under the MIT license. See LICENSE file.
 
 alias xcopen="xcfile() { local workspace=$(find *.xcworkspace 2>/dev/null | head -1); local project=$(find *.xcodeproj 2>/dev/null | head -1); \${workspace:-\${project}} ; }; open -a Xcode \"${xcfile}\""
+alias xcwhich="xcode-select --print-path"
 alias cartupdate="carthage update --platform iOS --no-use-binaries"
 alias codesigndoc="bash -l -c '$(curl -sfL https://raw.githubusercontent.com/bitrise-tools/codesigndoc/master/_scripts/install_wrap.sh)'"
 alias fl="fastlane"
@@ -16,4 +17,8 @@ simrec() {
 # usage: $ simurl http://apple.com
 simurl() {
   xcrun simctl openurl booted "$1"
+}
+# usage: $ xcswitch /Applications/Xcode-beta.app
+xcswitch() {
+  sudo xcode-select --switch "$1"
 }
