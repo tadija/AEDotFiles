405aa254636532ac9a6dd296fa32d30086a746f6
Marko Tadic <tadija@me.com>
2025-10-31 16:19:53 +0100

    Add a few fixes for WSL on Windows

--- plugins/nvim/lua/config/autocmds.lua
+++ plugins/nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd({ "TextYankPost", "TextChanged", "TextChangedI" }, {
+  callback = function()
+    -- only run if buffer actually has CR characters
+    if vim.fn.search("\r", "nw") ~= 0 then
+      vim.cmd([[%s/\r//ge]])
+    end
+  end,
+})
+
